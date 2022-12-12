c2c015185eee55be8e4ccd9c399cef6db8ecc61f
Thurcavalcante <arthurcavalcantethur@gmail.com>
2022-12-12 14:51:27 -0300

    atualizações do projeto

--- virtual/Scripts/activate.bat
+++ virtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Repositorios_Git\Projeto-Integrador-I\virtual
+set VIRTUAL_ENV=C:\Projeto-Integrador-I\virtual
 
 if not defined PROMPT set PROMPT=$P$G
 
