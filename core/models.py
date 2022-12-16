8a3bd0dc91b322a0d555c709b9ac39db306dbaff
Jose Moreira Chacon <josemoreirachacon06@gmail.com>
2022-12-16 15:17:13 -0300

    atualização

--- core/models.py
+++ core/models.py
@@ -5,23 +5,23 @@ from django.contrib.auth.models import AbstractUser #Padrão
 class Categoria(models.Model):
     nome = models.CharField("Nome", max_length=100)
 
-class Usuario(AbstractUser):# Vai conter no BD, uma sequecia de colunas padrão + os campos criados abaixo.
+class Usuario(AbstractUser):
     nome = models.CharField('Nome', max_length=100)
     idade = models.IntegerField('Idade')
     cpf = models.CharField('CPF', max_length=11, unique=True, primary_key=True) #unique não permite que tenha 2 cadastros com o memso dado, nesse caso, o CPF.
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+   # categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
 
-    USERNAME_FIELD = 'cpf' #A partir de agora o usuario deverá logar com o CPF, e não mais com o usuario.
+    USERNAME_FIELD = 'cpf' 
 
 
-    class Meta: #Definindo permissões que o usuario poderá assumir 
+    class Meta:  
         permissions = [
             ("permissao_adm_1", "podera acessar a view da pagina usuarios.html"),
             ("permissao_adm_2", "podera visualizar o privado do painel.html")
         ]
 
 class Historico_Consumo(models.Model):
-    nome = models.CharField("Nome", max_length=100) #Altertar para os campos corretos
+    nome = models.CharField("Nome", max_length=100) #Alterar para os campos corretos
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
 
 
